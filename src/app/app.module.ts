12560732cd7cb483a744913ec8728679cb655f8a
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-22 20:07:35 +0300

    add aplication routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule }   from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AsyncLocalStorageModule } from 'angular-async-local-storage';
 
@@ -9,6 +10,16 @@ import { PageCategoriesComponent } from './components/page-categories/page-categ
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 import { EditCategoryComponent } from './components/edit-category/edit-category.component';
+import { PageLocationsComponent } from './components/page-locations/page-locations.component';
+
+const appRoutes: Routes = [
+  { path: 'category', component: PageCategoriesComponent },
+  { path: 'location', component: PageLocationsComponent },
+  { path: '',
+    redirectTo: '/category',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -17,11 +28,16 @@ import { EditCategoryComponent } from './components/edit-category/edit-category.
     TopBarComponent,
     CategoriesListComponent,
     EditCategoryComponent,
+    PageLocationsComponent,
   ],
   imports: [
     BrowserModule,
     AsyncLocalStorageModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
