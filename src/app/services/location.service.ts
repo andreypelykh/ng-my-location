d368f13cc1ae0990465b3adc0c92f717c722f9f8
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-23 22:49:19 +0300

    finish location service

--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,17 +1,66 @@
 import { Injectable } from '@angular/core';
 import { AsyncLocalStorage } from 'angular-async-local-storage';
+import { Location, LocationProperties } from '../models/location.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable()
 export class LocationService {
+  private locations = new BehaviorSubject<Location[]>([]);
+  private currentValue: Location[];
  
-  public constructor(protected storage: AsyncLocalStorage) {}
- 
-  public ngOnInit() {
- 
-    this.storage.setItem('lang', 'fr').subscribe(() => {
-      // Done
-    });
- 
+  public constructor(protected storage: AsyncLocalStorage) {
+		this.storage.getItem('locations').subscribe((locations:Location[]) => {
+	    this.locations.next(locations);
+		});
+
+	  this.locations.subscribe(e => {
+	    this.currentValue = e;
+	  });
+  }
+
+  public getLocations(): Observable<Location[]> {
+  	return this.locations.asObservable();
+  }
+
+  public addLocation(location: Location) {
+
+    const newArray = [
+    	...this.currentValue,
+    	location
+    ];
+
+    this.updateStore(newArray);
+  }
+
+  public removeLocation(location: Location) {
+
+  	const pos = this.currentValue.indexOf(location);
+
+  	const newArray = [
+      ...this.currentValue.slice(0, pos),
+      ...this.currentValue.slice(pos + 1),
+    ];
+
+    this.updateStore(newArray);
+  }
+
+  public changeLocation(location: Location, locationChanges: LocationProperties) {
+    const changedLocation = Object.assign({}, location, locationChanges);
+
+    const pos = this.currentValue.indexOf(location);
+
+    const newArray = [
+      ...this.currentValue.slice(0, pos),
+      changedLocation,
+      ...this.currentValue.slice(pos + 1),
+    ];
+
+    this.updateStore(newArray);
+  }
+
+  private updateStore(newValue) {
+    this.locations.next(newValue);
+    this.storage.setItem('locations', newValue).subscribe();
   }
  
 }
