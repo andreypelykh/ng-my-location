d6d7881efb5535a1ea1d8eede7c21499d368d32b
Andrey Pelykh <ya.cool.pacan@gmail.com>
2017-09-23 23:56:03 +0300

    add main functionality page-locations components

--- src/app/components/page-locations/page-locations.component.ts
+++ src/app/components/page-locations/page-locations.component.ts
@@ -1,15 +1,64 @@
 import { Component, OnInit } from '@angular/core';
+import { Location, LocationProperties } from '../../models/location.model';
+import { LocationService } from '../../services/location.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-page-locations',
   templateUrl: './page-locations.component.html',
-  styleUrls: ['./page-locations.component.css']
+  styleUrls: ['./page-locations.component.css'],
+  providers: [ LocationService ]
 })
 export class PageLocationsComponent implements OnInit {
+	selectedLocation: Location = null;
+	locations: Observable<Location[]>;
+  isEditing: boolean = false;
+	isAdding: boolean = false;
 
-  constructor() { }
+
+  constructor(private locationService: LocationService) { }
 
   ngOnInit() {
+  	this.locations = this.locationService.getLocations();
+  }
+
+  onSelected(location: Location) {
+    if (this.selectedLocation === location) {
+      this.selectedLocation = null;
+    }
+    else {
+  	  this.selectedLocation = location;
+    }
+  }
+
+  onAddingSaved(properties: LocationProperties) {
+
+    this.locationService.addLocation(new Location(
+    	properties.name,
+    	properties.address,
+    	properties.coordinates,
+    	properties.categoryId
+    ));
+  }
+
+  onEditingSaved(properties: LocationProperties) {
+    this.locationService.changeLocation(this.selectedLocation, properties);
+  }
+
+  onAddCategory() {
+    this.isEditing = false;
+    this.isAdding = !this.isAdding;
+  }
+
+  onEditCategory() {
+    if (this.selectedLocation) {
+      this.isAdding = false;
+      this.isEditing = !this.isEditing;
+    }
+  }
+
+  removeCategory() {
+    this.selectedLocation && this.locationService.removeLocation(this.selectedLocation);
   }
 
 }
